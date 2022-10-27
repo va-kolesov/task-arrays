c2502ed5174ac656f1500d8cb9bfeb3570b5b155
va.kolesov <va.kolesov@tensor.ru>
2022-10-27 20:07:34 +0300

    va-kolesov/task-arrays

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,32 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+    return data
+        .split(',')
+        .map((str) => parseInt(str))
+        .sort((a, b) => a - b)
+        .join(',');
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+    return data.filter((num) => num <= 100);
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    const array = [];
+    array1.forEach((num, i) => {
+        array.push(num, array2[i]);
+    });
+    return array;
+}
